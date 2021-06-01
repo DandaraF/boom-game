6c47ac0a2c58428d5568d8a25833abcaa03df5b5
DandaraF <dandara-chr@hotmail.com>
2021-06-01 20:03:03 -0300

    alteração na função para remover balão

--- main.js
+++ main.js
@@ -4,39 +4,40 @@
 
 const containerBaloes = document.querySelector(".container-baloes");
 
+
 function adicionarBalao() {
+
   const elementoImg = document.createElement("img");
 
   elementoImg.setAttribute("src", "./assets/baloon.png");
   elementoImg.setAttribute("class", "balao");
-  elementoImg.setAttribute("id", "balao");
-  elementoImg.setAttribute("onclick", "removerBalao()");
+  // elementoImg.setAttribute("id", "balao");
+  // elementoImg.setAttribute("onclick", "removerBalao()");
   
-  
-
   const valorLeft = Math.round(Math.random() * 90);
   const valorTop = Math.round(Math.random() * 90);
 
   elementoImg.style.left = valorLeft + "vw";
   elementoImg.style.top = valorTop + "vh";
 
-  console.log("left=>", valorLeft);
-  console.log("Top=>", valorTop);
 
+  elementoImg.addEventListener("click", () => {
+    containerBaloes.removeChild(elementoImg);
+  })
   containerBaloes.appendChild(elementoImg);
 
-  console.log("Elemento Img =>", elementoImg);
+
+
 }
 
 setInterval(adicionarBalao, 3000); //3000 milesegundos = 3 segundos
 
 
-function removerBalao(elemento) {
+  // var no = document.getElementById("balao"); //está pegando o 1 elemento
+  // var elemento = document.imag
+  // console.log(" ==========>   ", elemento);
+// if (no.parentNode) {
+//   no.parentNode.removeChild(no);
+// }
 
-  var no = document.getElementById("balao");
-  console.log(" ==========>   ", no);
-if (no.parentNode) {
-  no.parentNode.removeChild(no);
-}
-}
   
